970970625e50f0af467dfa3500b4959c1303d3c9
rossijuan <jarossi06@gmail.com>
2013-05-06 23:30:08 -0300

    test

---
+++ wordpress/wp-content/themes/inmo/header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>40107 template preview</title>
+        <meta charset="utf-8">
+        <meta name="description" content="Your description">
+        <meta name="keywords" content="Your keywords">
+        <meta name="author" content="Your name">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" >
+        <link rel="icon" href="<? echo IMAGES ?>/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="<?php echo IMAGES ?>/favicon.ico" type="image/x-icon" />
+        <script src="<?php bloginfo('template_directory'); ?>/js/jquery-1.7.1.min.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery-1.7.1.min.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/script.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/superfish.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery.hoverIntent.minified.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/tms-0.4.1.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/uCarousel.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery.easing.1.3.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery.tools.min.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery.jqtransform.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery.quicksand.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery.snippet.min.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery-ui-1.8.17.custom.min.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery.cycle.all.min.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery.cookie.js"></script>
+
+        <script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
+        <!--[if lt IE 8]>
+           <div style=' clear: both; text-align:center; position: relative;'>
+             <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+               <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+            </a>
+          </div>
+        <![endif]-->
+        <!--[if lt IE 9]>
+                <script src="js/html5.js"></script>
+                <link rel="stylesheet" href="css/ie.css"> 
+        <![endif]-->
+    </head>
+    <body>
+        <!-- Header -->
+        <header>
+            <div class="header-bg">
+                <div class="main wrap">
+                    <h1><a href="index.html"><img src="<?php echo IMAGES ?>/logo.png" alt=""></a></h1>
+                    <div class="header-contacts">
+                        1-800-555-1234<br><a href="#">www.realestate@demolink.org</a>
+                    </div>
+                </div> 
+            </div>  
+            <nav>
+                <div class="main">
+                    <ul class="sf-menu">
+                        <li class="current"><a href="index.html">Home</a>
+                            <ul>
+                                <li><a href="more.html">Company</a></li>
+                                <li><a href="more.html">Team</a></li>
+                                <li><a href="more.html">News</a>
+                                    <ul>
+                                        <li><a href="more.html">Offers</a></li>
+                                        <li><a href="more.html">Properties</a></li>
+                                        <li><a href="more.html">Services</a></li>
+                                        <li><a href="more.html">Sale</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="more.html">Promotions</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="index-1.html">About</a></li>
+                        <li><a href="index-2.html">Services</a></li>
+                        <li><a href="index-3.html">Appraisals</a></li>
+                        <li><a href="index-4.html">Contact</a></li>
+                    </ul>  
+                    <form id="search-form" method="post">
+                        <input type="text" value="Search..." onBlur="if(this.value=='') this.value='Search...'" onFocus="if(this.value =='Search...' ) this.value=''"  />
+                        <a href="#" onClick="document.getElementById('search-form').submit()"></a>
+                    </form>
+                    <div class="clear"></div>
+                </div>
+            </nav>        
+        </header>
+        <!-- Content -->
+        <section id="content">
+
---
+++ wordpress/wp-content/themes/inmo/index.php
@@ -0,0 +1,218 @@
+<?php get_header();?>
+    <div class="container_12">
+      <div class="grid_12">
+          <div id="slide">
+            <div class="slider">
+                <ul class="items">
+                    <li><img src="<?php echo IMAGES?>/slider-1.jpg" alt="" /><div class="banner"><strong>$40000</strong><p><a href="more.html">Fresh view House</a>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu odio lacus. Ut rhoncus, dolor non rhoncus faucibus.</p></div></li>
+                    <li><img src="<?php echo IMAGES?>/slider-2.jpg" alt="" /><div class="banner"><strong>$48000</strong><p><a href="more.html">Fresh view House</a>Nullam eu odio lacus. Ut rhoncus, dolor non rhoncus faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p></div></li>
+                    <li><img src="<?php echo IMAGES?>/slider-3.jpg" alt="" /><div class="banner"><strong>$35000</strong><p><a href="more.html">Fresh view House</a>Ut rhoncus, dolor non rhoncus faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu odio lacus. </p></div></li>
+                </ul>
+                <a href="#" class="prev"></a>
+             	<a href="#" class="next"></a>
+             </div>	
+           </div>  
+           <div class="form-bg">
+              <strong>Find Your Home</strong>
+              <form id="form1" class="form-style" method="post">
+                  <label><span>Property</span>
+                      <strong class="select1">
+                          <select name="select">
+                              <option>--Select Type--</option>
+                              <option>...</option>
+                          </select>
+                      </strong>
+                  </label>
+                  <label>
+                      <span>Price</span>
+                      <strong class="select1">
+                          <select name="select">
+                              <option>--Select Type--</option>
+                              <option>...</option>
+                          </select>
+                      </strong>
+                  </label>
+                  <label><span>Region</span>
+                      <strong class="select1">
+                          <select name="select">
+                              <option>--Select Type--</option>
+                              <option>...</option>
+                          </select>
+                      </strong>
+                  </label>
+                  <label>
+                      <span>Rooms</span>
+                      <strong class="select1">
+                          <select name="select">
+                              <option>--Select Type--</option>
+                              <option>...</option>
+                          </select>
+                      </strong>
+                  </label>
+                  <label><span>Keyword</span><input type="text"></label>
+                  <div class="btns">
+                      <a onClick="document.getElementById('form1').submit()" href="#" class="button-1">Search</a>
+                  </div>
+                </form>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="grid_4">
+            <h4 class="h4-border p2">Perfect <span>Services</span></h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscingsil elit. Vestibulum vestibulum tincidunt orci, nec volutpatli dolor sollicitudin sed. Fusce temph or sodales massa ac cursus. Donec congue eleifejf rpnd est eu placerat. Suspendisse gravida turpis at metus.</p>
+      	</div>
+        <div class="grid_4">
+            <h4 class="h4-border p2">Global <span>Solutions</span></h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscingsil elit. Vestibulum vestibulum tincidunt orci, nec volutpatli dolor sollicitudin sed. Fusce temph or sodales massa ac cursus. Donec congue eleifejf rpnd est eu placerat. Suspendisse gravida turpis at metus.</p>
+      	</div>
+        <div class="grid_4">
+            <h4 class="h4-border p2">Property <span>Management</span></h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscingsil elit. Vestibulum vestibulum tincidunt orci, nec volutpatli dolor sollicitudin sed. Fusce temph or sodales massa ac cursus. Donec congue eleifejf rpnd est eu placerat. Suspendisse gravida turpis at metus.</p>
+      	</div>
+        <div class="grid_12 top">
+            <h4 class="h4-border p2">Featured <span>Listings</span></h4>
+            <div class="carousel-1-block">
+                  <div class="carousel-1">
+                      <ul>
+                          <li>
+                          	<div class="box-1">
+                            	<a href="more.html"><img src="<?php echo IMAGES?>/page1-img1.jpg" alt=""></a>
+                                <p class="clr-1">4545 Sabadell St<br>Las Vegas, NV 89212</p>
+                                <div><strong>Beds:</strong><span>7 bed</span></div>
+                                <div><strong>Baths:</strong><span>5 bath</span></div>
+                                <div><strong>House size:</strong><span>2,700 Sq Ft</span></div>
+                                <div><strong>Lot size:</strong><span>0.19 Acres</span></div>
+                            </div>
+                          </li>
+                          <li>
+                          	<div class="box-1">
+                            	<a href="more.html"><img src="<?php echo IMAGES?>/page1-img2.jpg" alt=""></a>
+                                <p class="clr-1">4545 Sabadell St<br>Las Vegas, NV 89212</p>
+                                <div><strong>Beds:</strong><span>7 bed</span></div>
+                                <div><strong>Baths:</strong><span>5 bath</span></div>
+                                <div><strong>House size:</strong><span>2,700 Sq Ft</span></div>
+                                <div><strong>Lot size:</strong><span>0.19 Acres</span></div>
+                            </div>
+                          </li>
+                          <li>
+                          	<div class="box-1">
+                            	<a href="more.html"><img src="<?php echo IMAGES?>/page1-img3.jpg" alt=""></a>
+                                <p class="clr-1">4545 Sabadell St<br>Las Vegas, NV 89212</p>
+                                <div><strong>Beds:</strong><span>7 bed</span></div>
+                                <div><strong>Baths:</strong><span>5 bath</span></div>
+                                <div><strong>House size:</strong><span>2,700 Sq Ft</span></div>
+                                <div><strong>Lot size:</strong><span>0.19 Acres</span></div>
+                            </div>
+                          </li>
+                          <li>
+                          	<div class="box-1">
+                            	<a href="more.html"><img src="<?php echo IMAGES?>/page1-img4.jpg" alt=""></a>
+                                <p class="clr-1">4545 Sabadell St<br>Las Vegas, NV 89212</p>
+                                <div><strong>Beds:</strong><span>7 bed</span></div>
+                                <div><strong>Baths:</strong><span>5 bath</span></div>
+                                <div><strong>House size:</strong><span>2,700 Sq Ft</span></div>
+                                <div><strong>Lot size:</strong><span>0.19 Acres</span></div>
+                            </div>
+                          </li>
+                          <li>
+                          	<div class="box-1">
+                            	<a href="more.html"><img src="<?php echo IMAGES?>/page1-img1.jpg" alt=""></a>
+                                <p class="clr-1">4545 Sabadell St<br>Las Vegas, NV 89212</p>
+                                <div><strong>Beds:</strong><span>7 bed</span></div>
+                                <div><strong>Baths:</strong><span>5 bath</span></div>
+                                <div><strong>House size:</strong><span>2,700 Sq Ft</span></div>
+                                <div><strong>Lot size:</strong><span>0.19 Acres</span></div>
+                            </div>
+                          </li>
+                          <li>
+                          	<div class="box-1">
+                            	<a href="more.html"><img src="<?php echo IMAGES?>/page1-img2.jpg" alt=""></a>
+                                <p class="clr-1">4545 Sabadell St<br>Las Vegas, NV 89212</p>
+                                <div><strong>Beds:</strong><span>7 bed</span></div>
+                                <div><strong>Baths:</strong><span>5 bath</span></div>
+                                <div><strong>House size:</strong><span>2,700 Sq Ft</span></div>
+                                <div><strong>Lot size:</strong><span>0.19 Acres</span></div>
+                            </div>
+                          </li>
+                          <li>
+                          	<div class="box-1">
+                            	<a href="more.html"><img src="<?php echo IMAGES?>/page1-img3.jpg" alt=""></a>
+                                <p class="clr-1">4545 Sabadell St<br>Las Vegas, NV 89212</p>
+                                <div><strong>Beds:</strong><span>7 bed</span></div>
+                                <div><strong>Baths:</strong><span>5 bath</span></div>
+                                <div><strong>House size:</strong><span>2,700 Sq Ft</span></div>
+                                <div><strong>Lot size:</strong><span>0.19 Acres</span></div>
+                            </div>
+                          </li>
+                          <li>
+                          	<div class="box-1">
+                            	<a href="more.html"><img src="<?php echo IMAGES?>/page1-img4.jpg" alt=""></a>
+                                <p class="clr-1">4545 Sabadell St<br>Las Vegas, NV 89212</p>
+                                <div><strong>Beds:</strong><span>7 bed</span></div>
+                                <div><strong>Baths:</strong><span>5 bath</span></div>
+                                <div><strong>House size:</strong><span>2,700 Sq Ft</span></div>
+                                <div><strong>Lot size:</strong><span>0.19 Acres</span></div>
+                            </div>
+                          </li>
+                      </ul>
+                  </div>
+                  <a href="#" class="carousel-prev carousel-1-button" data-type="prevPage"><span></span></a>
+                  <a href="#" class="carousel-next carousel-1-button" data-type="nextPage"><span></span></a>
+             </div>             
+        </div>  
+        <div class="grid_12"><div class="border-1 bot-1"></div></div>  
+        <div class="grid_2 box-2">
+        	<span>Company</span>
+            <ul class="list-1">
+            	<li><a href="index-1.html">About Us</a></li>
+            	<li><a href="index-2.html">Services</a></li>
+            	<li><a href="more.html">Clients</a></li>
+                <li><a href="more.html">Presentation</a></li>
+            </ul>
+        </div>
+        <div class="grid_2 box-2">
+        	<span>For Clients</span>
+            <ul class="list-1">
+            	<li><a href="more.html">Sign Up</a></li>
+            	<li><a href="more.html">Forum</a></li>
+                <li><a href="more.html">Promotions</a></li>
+            </ul>
+        </div>
+        <div class="grid_2 box-2">
+        	<span>Advice</span>
+            <ul class="list-1">
+            	<li><a href="more.html">Support</a></li>
+            	<li><a href="more.html">FAQs</a></li>
+            	<li><a href="more.html">Website</a></li>
+            </ul>
+        </div>
+        <div class="grid_3 box-2">
+        	<div>
+                <span>Properties</span>
+                <ul class="list-1">
+                    <li><a href="more.html">Luxury</a></li>
+                    <li><a href="more.html">Residential</a></li>
+                    <li><a href="more.html">Commercial</a></li>
+                    <li><a href="more.html">Budget</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="grid_3">
+        	<div class="newsletter">
+                <span>Newsletter Sign Up:</span>
+                <form id="form2" method="post">
+                  <input type="text" value="Enter your email here..." onBlur="if(this.value=='') this.value='Enter your email here...'" onFocus="if(this.value =='Enter your email here...' ) this.value=''"  />
+                  <a href="#" onClick="document.getElementById('form2').submit()">Go!</a>
+                </form>
+            </div>    
+        </div>
+      	<div class="clear"></div>
+    </div>  
+</section>
+<!-- Footer -->
+	<footer>
+    	Real Estate © 2012 <a href="index-5.html">Privacy Policy</a>&nbsp;<!-- {%FOOTER_LINK} -->
+    </footer>
+<!-- Footer -->
+</body>
+</html>
